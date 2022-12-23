46280854706295493a80d2362e9763aaeb988587
Jacob Shalabi <jshalabi03@gmail.com>
2022-12-23 13:19:51 -0600

    Initial Commit

---
+++ francescas_ingredient_finder/francescas_ingredient_finderApp.swift
@@ -0,0 +1,17 @@
+//
+//  francescas_ingredient_finderApp.swift
+//  francescas_ingredient_finder
+//
+//  Created by Jacob Shalabi on 12/23/22.
+//
+
+import SwiftUI
+
+@main
+struct francescas_ingredient_finderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

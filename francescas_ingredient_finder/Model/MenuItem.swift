9987d4bdfb7f0efd0a381f7f9db653a832c98667
Jacob Shalabi <jshalabi03@gmail.com>
2022-12-24 11:38:14 -0600

    Load functionality for model data

---
+++ francescas_ingredient_finder/Model/MenuItem.swift
@@ -0,0 +1,13 @@
+//
+//  MenuItem.swift
+//  francescas_ingredient_finder
+//
+//  Created by Jacob Shalabi on 12/24/22.
+//
+
+import Foundation
+
+struct MenuItem: Codable {
+    var name: String
+    var description: String
+}
---
+++ francescas_ingredient_finder/Model/ModelData.swift
@@ -0,0 +1,32 @@
+//
+//  ModelData.swift
+//  francescas_ingredient_finder
+//
+//  Created by Jacob Shalabi on 12/24/22.
+//
+
+import Foundation
+
+var menu_items: [MenuItem] = load("menu.json")
+
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+    
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+    else {
+        fatalError("Couldn't find \(filename) in main bundle.")
+    }
+    
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+    }
+    
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+    }
+}
